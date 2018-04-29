ccad099891ddf16d79f3b37c2d097b69474ea851
Jingbelle <35607963+Jingbelle@users.noreply.github.com>
2018-04-28 22:10:11 -0600

    Add files via upload

---
+++ src/components/movie.js
@@ -0,0 +1,56 @@
+import React, { Component }  from 'react';
+import {connect} from "react-redux";
+import { Glyphicon, Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Image } from 'react-bootstrap'
+import { withRouter } from "react-router-dom";
+import {fetchMovie} from "../actions/movieActions";
+
+//support routing by creating a new component
+
+class Movie extends Component {
+
+    componentDidMount() {
+        const {dispatch} = this.props;
+        if (this.props.selectedMovie == null)
+            dispatch(fetchMovie(this.props.movieId));
+    }
+
+    render() {
+
+
+
+
+        const DetailInfo = ({currentMovie}) => {
+            if (!currentMovie) { // evaluates to true if currentMovie is null
+                return <div>Loading...</div>;
+            }
+            return (
+                <Panel>
+                    <Panel.Heading>Movie Detail</Panel.Heading>
+                    <Panel.Body><Image className="image" src={currentMovie.url} thumbnail /></Panel.Body>
+                    <ListGroup>
+                        <ListGroupItem>{currentMovie.Title}</ListGroupItem>
+                        <ListGroupItem>{currentMovie._id}</ListGroupItem>
+
+                        <ListGroupItem><h4><Glyphicon glyph={'star'} /> {currentMovie.Rating} </h4></ListGroupItem>
+                    </ListGroup>
+
+                    <Panel.Body>{currentMovie.Reviews} </Panel.Body>
+                </Panel>
+            );
+        };
+        return (
+            <DetailInfo currentMovie={this.props.selectedMovie} />
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    console.log(ownProps);
+    return {
+        selectedMovie: state.movie.selectedMovie,
+        movieId: ownProps.match.params.movieId
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(Movie));
